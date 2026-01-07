52ce58c4a84b01d05844f74d1be1d5056df5f701
Lorenzo Ortellini <ortellini.lorenzoalberto.2024@itisgalileiroma.it>
2026-01-07 09:26:42 +0100

    Add files via upload

---
+++ moltiplicazione.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, p;
+    
+    // Primo numero
+    cout << "Inserisci il primo numero: ";
+    cin >> x;
+    
+    // Secondo numero
+    cout << "Inserisci il secondo numero: ";
+    cin >> y;
+    
+    // Risultato
+    p = x * y;
+    cout << "Il prodotto e': " << p << endl;
+    
+    return 0;
+}
